826d0c0c6cba84887bfbc4fe6b69423a2851a3d6
hanappe <peter@hanappe.com>
2024-02-14 20:45:45 +0100

    Adding analog measurements

--- LEDControl/LEDControl.ino
+++ LEDControl/LEDControl.ino
@@ -24,6 +24,7 @@
 #include <RomiSerialErrors.h>
 #include <stdint.h>
 #include "DigitalPulse.h"
+#include "AnalogMeasure.h"
 #include "ActivityManager.h"
 #include "BoardFactory.h"
 #include "ITimer.h"
@@ -37,10 +38,16 @@ void send_info(IRomiSerial *romiSerial, int16_t *args, const char *string_arg);
 void handle_add_digital_pulse(IRomiSerial *romiSerial, int16_t *args,
                               const char *string_arg);
 void handle_set_secondary(IRomiSerial *romiSerial, int16_t *args, const char *string_arg);
+void handle_add_analog_measure(IRomiSerial *romiSerial, int16_t *args,
+                                 const char *string_arg);
 void handle_stop_measurements(IRomiSerial *romiSerial, int16_t *args,
                               const char *string_arg);
-void handle_start_mesurements(IRomiSerial *romiSerial, int16_t *args,
+void handle_start_measurements(IRomiSerial *romiSerial, int16_t *args,
                               const char *string_arg);
+void handle_count_measurements(IRomiSerial *romiSerial, int16_t *args,
+                              const char *string_arg);
+void handle_get_measurement(IRomiSerial *romiSerial, int16_t *args,
+                            const char *string_arg);
 void handle_add_relay(IRomiSerial *romiSerial, int16_t *args, const char *string_arg);
 void handle_is_active(IRomiSerial *romiSerial, int16_t *args, const char *string_arg);
 void handle_reset(IRomiSerial *romiSerial, int16_t *args, const char *string_arg);
@@ -48,9 +55,12 @@ void handle_reset(IRomiSerial *romiSerial, int16_t *args, const char *string_arg
 const static MessageHandler handlers[] = {
         { 'd', 8, false, handle_add_digital_pulse },
         { 'r', 1, false, handle_add_relay },
+        { 'a', 7, false, handle_add_analog_measure },
         { 's', 2, false, handle_set_secondary },
-        { 'b', 2, false, handle_start_mesurements },
+        { 'b', 2, false, handle_start_measurements },
         { 'e', 0, false, handle_stop_measurements },
+        { 'n', 0, false, handle_count_measurements },
+        { 'g', 0, false, handle_get_measurement },
         { 'R', 0, false, handle_reset },
         { 'A', 0, false, handle_is_active },
         { '?', 0, false, send_info },
@@ -78,6 +88,9 @@ RomiSerial romiSerial(serial, serial, handlers, sizeof(handlers) / sizeof(Messag
 ActivityManager activityManager;
 
 extern volatile int32_t timer_interrupts_;
+
+CircularBuffer measurement_buffer;
+
 
 void setup()
 {
@@ -152,8 +165,94 @@ void handle_set_secondary(IRomiSerial *romiSerial, int16_t *args, const char *st
         }
 }
 
-void handle_start_mesurements(IRomiSerial *romiSerial, int16_t *args,
-                              const char *string_arg)
+void handle_add_analog_measure(IRomiSerial *romiSerial, int16_t *args,
+                               const char *string_arg)
+{        
+        int8_t pin = (uint8_t) args[0];
+        int8_t analog_pin = number_to_analog_pin(pin);
+        int32_t start_delay = (int32_t) args[1] * 1000 + args[2];
+        int32_t duration = (int32_t) args[3] * 1000 + args[4];
+        int32_t period = (int32_t) args[5] * 1000 + args[6];
+        
+        if (analog_pin == -1)
+                romiSerial->send_error(kBadPinCode, kBadPinMessage);
+        else if (start_delay < 0)
+                romiSerial->send_error(kBadStartCode, kBadStartMessage);
+        else if (period <= 0)
+                romiSerial->send_error(kBadPeriodCode, kBadPeriodMessage);
+        else if (duration < 0 || duration > period)
+                romiSerial->send_error(kBadDurationCode, kBadDurationMessage);
+        else if (activityManager.availableSpace() == 0) 
+                romiSerial->send_error(kMaxActivitiesCode, kMaxActivitiesMessage);
+        else {
+                auto activity = new AnalogMeasure(measurement_buffer, analog_pin, pin, start_delay, period, duration);
+                activityManager.addActivity(activity);
+                romiSerial->send_ok();
+        }
+}
+
+int8_t number_to_analog_pin(int8_t number)
+{
+        int8_t analog_pin;
+        switch (number) {
+        default:
+                analog_pin = -1;
+                break;
+        case 0:
+                analog_pin = A0;
+                break;
+        case 1:
+                analog_pin = A1;
+                break;
+        case 2:
+                analog_pin = A2;
+                break;
+        case 3:
+                analog_pin = A3;
+                break;
+        case 4:
+                analog_pin = A4;
+                break;
+        case 5:
+                analog_pin = A5;
+                break;
+#if defined(A6)
+        case 6:
+                analog_pin = A6;
+                break;
+#endif
+#if defined(A7)
+        case 7:
+                analog_pin = A7;
+                break;
+#endif
+#if defined(A8)
+        case 8:
+                analog_pin = A8;
+                break;
+#endif
+#if defined(A9)
+        case 9:
+                analog_pin = A9;
+                break;
+#endif
+#if defined(A10)
+        case 10:
+                analog_pin = A10;
+                break;
+#endif
+#if defined(A11)
+        case 11:
+                analog_pin = A11;
+                break;
+#endif
+        }
+        
+        return analog_pin;
+}
+
+void handle_start_measurements(IRomiSerial *romiSerial, int16_t *args,
+                               const char *string_arg)
 {
         int32_t duration = (int32_t) args[0] * 1000 + args[1];
         if (duration > 0) {
@@ -205,4 +304,28 @@ void handle_reset(IRomiSerial *romiSerial, int16_t *args, const char *string_arg
         timer.stop();
         activityManager.clear();
         romiSerial->send_ok();
+}
+
+void handle_count_measurements(IRomiSerial *romiSerial, int16_t *args, const char *string_arg)
+{
+        int8_t n = measurement_buffer.available();
+        String r = String("[0,") + n + String("]");
+        romiSerial->send(r.c_str()); 
+}
+
+void handle_get_measurement(IRomiSerial *romiSerial, int16_t *args, const char *string_arg)
+{
+        if (measurement_buffer.available() > 0) {
+                AnalogMeasurement m;
+                measurement_buffer.get(m);
+                String r = (String("[0,")
+                            + m.pin_ + String(",")
+                            + m.timestamp_ + String(",")
+                            + m.count_ + String(",")
+                            + m.sum_ + String("]"));
+                romiSerial->send(r.c_str()); 
+                
+        } else {
+                romiSerial->send("[0,-1,0,0,0]"); 
+        }
 }
